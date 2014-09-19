0d4f85cced95979c33baad9343c132a76868b1a9
Armin Ronacher <armin.ronacher@active-4.com>
2014-09-19 07:50:38 +0200

    Reindenting in the allocation module.

--- src/alloc.c
+++ src/alloc.c
@@ -4,25 +4,30 @@
 
 #include <stdlib.h>
 
-static void *internal_malloc(void *ctx, size_t sz)
+
+static void *
+internal_malloc(void *ctx, size_t sz)
 {
     (void)ctx;
     return malloc(sz);
 }
 
-static void *internal_realloc(void *ctx, void *previous, size_t sz)
+static void *
+internal_realloc(void *ctx, void *previous, size_t sz)
 {
     (void)ctx;
     return realloc(previous, sz);
 }
 
-static void internal_free(void *ctx, void *ptr)
+static void
+internal_free(void *ctx, void *ptr)
 {
     (void)ctx;
     free(ptr);
 }
 
-void jhn__set_default_alloc_funcs(jhn_alloc_funcs_t *af)
+void
+jhn__set_default_alloc_funcs(jhn_alloc_funcs_t *af)
 {
     af->malloc_func = internal_malloc;
     af->free_func = internal_free;
